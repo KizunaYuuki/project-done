5ef6b21f51ada0469c4c40456bb87d39b84fde6a
KizunaYuuki <89437550+KizunaYuuki@users.noreply.github.com>
2023-04-18 16:54:23 +0700

    To do list - MEVN

---
+++ To do list/frontend/src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template >
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/" class="navbar-brand">Danh sách công việc </a>
+        <div class="mr-auto navbar-nav">
+            <li class="nav-item">
+                <router-link :to="{ name: 'contactbook' }" class="nav-link">
+                    Todolist
+                    <i class="fa-regular fa-rectangle-list"></i>
+                </router-link>
+            </li>
+        </div>
+    </nav>
+</template>
---
+++ To do list/frontend/src/components/ContactCard.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    props: {
+        contact: { type: Object, required: true },
+    },
+};
+</script>
+<template>
+    <div>
+        <div class="p-1">
+            <strong>Tên:</strong>
+            {{ contact.name }}
+        </div>
+        <div class="p-1">
+            <strong>Nội dung:</strong>
+            {{ contact.content }}
+        </div>
+        <div class="p-1">
+            <strong>Địa chỉ:</strong>
+            {{ contact.address }}
+        </div>
+        <div class="p-1">
+            <strong>Tags:</strong>
+            {{ contact.tags }}
+        </div>
+        <div class="p-1">
+            <strong>Hoàn thành&nbsp;</strong>
+            <i v-if="contact.complete" class="fas fa-check"></i>
+            <i v-else class="fas fa-times"></i>
+        </div>
+    </div>
+</template>
